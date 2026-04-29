a0f1252b0cb183de2794f523b934f24ef3ca8b7e
HANSONGYI <hhan1218@naver.com>
2026-04-30 02:54:00 +0900

    migration

--- prisma/migrations/20251005095508_add_public_record_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."FosterRecord" ADD COLUMN     "healthNote" TEXT;
-
--- AlterTable
-ALTER TABLE "public"."Organization" ADD COLUMN     "email" TEXT,
-ADD COLUMN     "zipcode" TEXT;
--- prisma/migrations/20251015230000_add_animal_foster_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Animal"
-ADD COLUMN     "euthanasiaDate" TIMESTAMP(3),
-ADD COLUMN     "isFosterCondition" BOOLEAN NOT NULL DEFAULT false;
